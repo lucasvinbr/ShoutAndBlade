0919b2b186e6b71888bee6adb2fd115069c37c11
Lucas Vinicius <lucasvinbr@gmail.com>
2025-09-10 23:57:25 -0300

    Add placeholder translations (and generator)

---
+++ utils/generate_translations.lua
@@ -0,0 +1,41 @@
+local lfs = require("lfs")
+
+local baseDir = "F:/proyects/ShoutAndBlade/"
+
+lfs.chdir(baseDir .. "Interface/translations/")
+
+local filePrefix = "ShoutAndBlade_"
+local baseTranslation = "ENGLISH"
+local fileSuffix = ".txt"
+
+local otherTranslations = {
+    "CHINESE",
+    "CZECH",
+    "FRENCH",
+    "GERMAN",
+    "ITALIAN",
+    "JAPANESE",
+    "POLISH",
+    "RUSSIAN",
+    "SPANISH",
+}
+
+local baseFile = io.open(filePrefix .. baseTranslation .. fileSuffix, "r")
+
+if baseFile then
+    local content = baseFile:read("a")
+
+    print(content)
+
+    for _, otherTrans in pairs(otherTranslations) do
+        local otherFile = io.open(filePrefix .. otherTrans .. fileSuffix, "w+b")
+        if otherFile then
+            otherFile:write(content)
+            otherFile:close()
+        end
+    end
+
+    baseFile:close()
+else
+    print("error: base translation file not found")
+end
